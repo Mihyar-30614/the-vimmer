3dd36eb96b430746ea226ba99a2d1af8e863364f
Mihyar Al Masalma <eng.mihyear@gmail.com>
2026-05-14 13:21:21 -0300

    feat(rooms): rewrite beginner_insert2 with header+items scenario

--- lua/rooms/beginner/insert2.lua
+++ lua/rooms/beginner/insert2.lua
@@ -1,16 +1,25 @@
--- Beginner room: A/o/O insert variants. Player appends "!" to 3 lines using A + j repeat.
+-- Beginner room: a/A/o/O. Add header above, append commas, add trailing item.
 return {
   id = "beginner_insert2",
   tier = "beginner",
   command = "a / A / o / O",
   title = "Insert Variants",
   description = "A = append at end of line. o = open line below. O = open line above.",
-  before_example = "hello|",
-  after_example = "hello!",
+  before_example = "one|\ntwo",
+  after_example = "items:|\none,\ntwo,\nthree",
   usage_tip = "A puts you at end of line in insert mode. o opens a new line below.",
-  start_text = "alpha\nbeta\ngamma",
-  target_text = "alpha!\nbeta!\ngamma!",
-  base_xp = 50,
+  filetype = "text",
+  cursor_start = { row = 1, col = 1 },
   time_limit = 60,
-  optimal_keystrokes = { "A", "!", "\27", "j", "A", "!", "\27", "j", "A", "!", "\27" },
+  goal = "Add header `items:` above, comma to lines 1+2, then `three` below.",
+  start_text = [[
+one
+two]],
+  target_text = [[
+items:
+one,
+two,
+three]],
+  base_xp = 55,
+  optimal_keystrokes = { "O", "i", "t", "e", "m", "s", ":", "\27", "j", "A", ",", "\27", "j", "A", ",", "\27", "o", "t", "h", "r", "e", "e", "\27" },
 }
